e3aede4dae67f96415ccb360380ef4fe05d4907f
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-25 16:44:08 +0200

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Figures
+{
+    public class Rectangle:Figure
+    {
+        private int b;
+        public Rectangle()
+        {
+            b = 0;
+        }
+        public Rectangle(int a,int b):base(a)
+        {
+            this.b = b;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * (a + b);
+        }
+        public override double CalculateArea()
+        {
+            return a * b;
+        }
+    }
+}
