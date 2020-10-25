12b7fff77a9e51b7b6f9bd355c8f7a8388b3003a
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-25 16:46:00 +0200

    Add files via upload

---
+++ Figures.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Figures
+{
+    class Program
+    {
+        static void Main()
+        {
+            Figure triangle = new Triangle(10, 20, 30);
+            Figure rectangle = new Rectangle(5, 10);
+            Figure circle = new Circle(5);
+            PrintFigureInfo(triangle);
+            PrintFigureInfo(rectangle);
+            PrintFigureInfo(circle);
+        }
+        static void PrintFigureInfo(Figure figure)
+        {
+            Console.WriteLine("{0} perimeter is:{1}", figure.GetType().Name, figure.CalculatePerimeter());
+            Console.WriteLine("{0} area is:{1}", figure.GetType().Name, figure.CalculateArea());
+        }
+    }
+}
