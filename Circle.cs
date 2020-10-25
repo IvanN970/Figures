2d177375c82ef3490a8b5a8f3ee50ddd82ab7510
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-25 16:43:31 +0200

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Figures
+{
+    public class Circle:Figure
+    {
+        public Circle(int a) : base(a) { }
+        public override double CalculatePerimeter()
+        {
+            return Math.Round(2 * Math.PI * a, 2);
+        }
+        public override double CalculateArea()
+        {
+            return Math.Round(Math.PI * Math.Pow(a, 2), 2);
+        }
+    }
+}
