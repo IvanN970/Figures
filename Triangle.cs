1be96650be3ab81ec4a2d47df2c2d5c46b8c34c3
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-25 16:44:43 +0200

    Add files via upload

---
+++ Triangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Figures
+{
+    public class Triangle:Figure
+    {
+        private int b;
+        private int c;
+        public Triangle()
+        {
+            b = 0;
+            c = 0;
+        }
+        public Triangle(int a,int b,int c):base(a)
+        {
+            this.b = b;
+            this.c = c;
+        }
+        public int B
+        {
+            set { b = value;}
+            get { return b;}
+        }
+        public int C
+        {
+            set { c = value;}
+            get { return c;}
+        }
+        public override double CalculatePerimeter()
+        {
+            return a + b + c;
+        }
+        public override double CalculateArea()
+        {
+            return (a * b) / 2;
+        }
+    }
+}
