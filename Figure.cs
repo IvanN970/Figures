4859f91ba66def7b43850ee5ea47855e0508c55f
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-25 16:45:26 +0200

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Figures
+{
+    public abstract class Figure
+    {
+        protected int a;
+        public Figure()
+        {
+            a = 0;
+        }
+        public Figure(int a)
+        {
+            this.a = a;
+        }
+        public int A
+        {
+            set {a = value;}
+            get {return a;}
+        }
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+            
+    }
+}
